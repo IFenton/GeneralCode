fbbd97789ac51f78e2dc77e5f987ee51d1dc90f6
Isabel Fenton <isabf@nhm.ac.uk>
2014-05-16 18:47:55 +0100

    a much faster way of doing this (removes loops within loops)

--- sp_mat_2_df.R
+++ sp_mat_2_df.R
@@ -1,7 +1,6 @@
 ## function for converting from a spatial matrix into a dataframe of Long, Lat and value
 ## created 10 / 01 / 2014
 ## Originally to convert chlorophyll a data into a usable format
-## this is not optimised at all (works ok for 1degree grid, much finer and it probably takes too long)
 ## the code starts at the top left corner of the matrix
 
 ## input: x.coords, y.coords (n.b. this must be in the correct order,e.g. positive to negative), spatial matrix
@@ -15,13 +14,11 @@ sp.mat.2.df <- function(x, y, sp.mat) {
   names(dat) <- c("Long", "Lat")
   
   # populate the new column
-  n <- 1
-  for (i in 1:nrow(sp.mat)){
-    for (j in 1:ncol(sp.mat)) {
-      dat$val[n] <- sp.mat[i, j]
-      n <- n + 1
-    }
+  tmp <- NULL
+  for (i in 1:ncol(sp.mat)){
+    tmp <- c(tmp, sp.mat[, i])
   }
+  dat$val <- tmp
   return(dat)
 }
 
