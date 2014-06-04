801e489e9a988ef6f2f172aec5c196cbda7940d8
Isabel Fenton <isabf@nhm.ac.uk>
2014-06-04 14:57:58 +0100

    Code to calculate and plot likelihood ratios

---
+++ lr_calculations.R
@@ -0,0 +1,147 @@
+## 30 / 5 / 2014
+## Isabel Fenton
+## Likelihood ratio plots
+##
+## Code to calculate likelihood ratios for a set of variables
+##
+## Functions: 
+## model.evs - a function to get the list of variables from a model, including poly
+## lr.calc - a function that calculates likelihood ratios for a set of variables
+##
+## To do: Initially set to run with sarlm models
+##        suspect this only works with one poly variable
+##        n.b. can't handle log()
+
+library(lmtest)
+
+model.evs <- function(model) {
+  # function to get the list of variables from a model, including poly
+  # input  - model: the model to work from
+  # output - a list of the variables
+  EVs <- all.vars(as.formula(model))[-1] # use the -1 to remove the RV
+  # if poly is present
+  if (length(grep("poly", names(model$coefficients), fixed = TRUE)) > 0) {
+    # identify where poly is present and it's not an interaction
+    warning("Only tested with one poly variable")
+    poly.ev <- names(model$coefficients)[setdiff(grep("poly", names(model$coefficients), fixed = TRUE), grep(":", names(model$coefficients), fixed = TRUE))]
+    
+    tmp.poly <- NULL
+    full.poly <- NULL
+    # identify the unique EVs from that list (uni.poly)
+    for (i in poly.ev){
+      tmp.poly <- c(tmp.poly, names(unlist(sapply(EVs, grep, i, fixed = TRUE))))
+    }
+    uni.poly <- unique(tmp.poly)
+    # for each unique poly generate a sequence from the max order to 1
+    for (i in uni.poly) {
+      # identify the max order of poly
+      tmp <- poly.ev[grep(i, poly.ev, fixed = TRUE)][1] # given these aren't interactions, these must have the highest order of poly, so arbitraily take the first one
+      poly.seq <- seq(as.numeric(substr(tmp, nchar(tmp) - 2, nchar(tmp) - 2)), 1)
+      for (j in poly.seq) {
+        substr(tmp, nchar(tmp) - 2, nchar(tmp) - 2) <- as.character(j)
+        full.poly <- c(full.poly, substr(tmp, 1, nchar(tmp) - 1))
+      }
+    }
+    # remove those EVs which are poly from the total EV list
+    EVs <- setdiff(EVs, uni.poly)
+    # add the poly values
+    EVs <- c(EVs, full.poly)
+  }
+  return(EVs)
+}
+
+
+lr.calc <- function(model, EVs = NULL) {
+  # function to calculate likelihood ratios
+  # input  - model: the model to work from
+  #        - EVs: dataframe of variables, and the groups they are in (optional, otherwise calculated from model)
+  # output - dataframe containing variables, p values and significance stars
+
+  # obtain a list of variables
+  if (is.null(EVs)) {
+    # calculate the list of EVs
+    EVs <- model.evs(model)
+    # create a dataframe of these, so it matches the input of groups
+    EVs <- data.frame(name = EVs, group = EVs)
+  } else {
+    # create a standard format
+    if(!is.data.frame(EVs)) stop("Expecting a dataframe")
+    if(ncol(EVs) != 2) stop ("Expecting 2 columns")
+    if(length(unique(EVs[, 1])) < length(unique(EVs[, 2]))) stop ("Order should be name, then group")
+    colnames(EVs) <- c("name", "group")
+  }
+  
+  # generate a dataframe to store these values
+  LRs <- data.frame(names = unique(EVs$group), lr = NA, p = NA, stars = NA)
+  
+  # for each value in group
+  for (i in unique(EVs$group)) {
+    # identify the variables to remove
+    m.EVs <- EVs$name[EVs$group == i]
+    # if it is poly need to add higher orders
+    if (length(grep("poly", i, fixed = TRUE)) > 0){
+      # identify the order
+      poly.order <- substr(i, regexpr(")", i) - 1, regexpr(")", i) - 1)
+      # if there are higher orders, then add those to m.EVs
+      # what polys are there
+      all.poly <- unique(EVs$name)[grep("poly", unique(EVs$name), fixed = TRUE)]
+      max.poly <- max(unlist(lapply(all.poly, function(x) substr(x, regexpr(")", x) - 1, regexpr(")", x) - 1))))
+      # if the maximum is greater than the one we have currently, add these higher ones in
+      if (poly.order < max.poly)
+        m.EVs <- c(as.character(m.EVs), as.character(all.poly[substr(all.poly, regexpr(")", all.poly) - 1, regexpr(")", all.poly) - 1) > substr(i, regexpr(")", i) - 1, regexpr(")", i) - 1)]))
+    }
+    # create all possible interaction variables
+    m.all.int <- unlist(lapply(m.EVs, function(x) names(model$coefficients)[grep(x, names(model$coefficients), fixed = TRUE)]))
+    # make sure these are the same as found in EVs$name
+    m.all.EVs <- NULL
+    for (j in 1:length(m.all.int)) {
+      tmp.split <- strsplit(m.all.int[j], ":")
+      # for each part, what is the EVs$name that contains that part
+      tmp.1 <- unlist(lapply(EVs$name, function(x) if (length(grep(x, tmp.split[[1]][1], fixed = TRUE)) > 0) as.character(x)))
+      if(length(tmp.split[[1]]) == 2) {
+        tmp.2 <- unlist(lapply(EVs$name, function(x) if (length(grep(x, tmp.split[[1]][2], fixed = TRUE)) > 0) as.character(x)))
+        tmp.1 <- paste(tmp.1, ":", tmp.2, sep= "")
+      }
+      m.all.EVs <- c(m.all.EVs, tmp.1)
+    }
+    m.all.EVs <- unique(m.all.EVs)
+    # if it is poly, need to add some in as well
+    # if the main variable is poly
+    if (length(grep("poly", i, fixed = TRUE)) > 0 ) {
+      # reduce the order by one for all poly
+      tmp.order <- as.numeric(min(unlist(lapply(m.EVs, function(x) substr(x, regexpr(")", x) - 1, regexpr(")", x) - 1)))))
+      if (tmp.order > 1) {
+        poly.add <- NULL
+        for (k in m.all.EVs) {
+          tmp.add <- as.numeric(substr(k, regexpr(")", k) - 1, regexpr(")", k) - 1))
+          # subtract one if the value is less than 1
+          if (tmp.add > 1) 
+            substr(k, regexpr(")", k) - 1, regexpr(")", k) - 1) <- as.character(tmp.order - 1)
+          poly.add <- c(poly.add, k)
+        }
+        tmp.mod <- eval(parse(text = paste("update(model, ~. -", paste(m.all.EVs, collapse = " - "), " + ", paste(poly.add, collapse = " + "), ")", sep = "")))
+      } else {
+        tmp.mod <- eval(parse(text = paste("update(model, ~. -", paste(m.all.EVs, collapse = " - "), ")", sep = "")))
+      }
+    } else {
+      # create a model with that variable(s) removed
+      tmp.mod <- eval(parse(text = paste("update(model, ~. -", paste(m.all.EVs, collapse = " - "), ")", sep = "")))
+    }
+    # obtain the LR and P-value
+    LRs$lr[LRs$names == i] <- lrtest(model, tmp.mod)$Chisq[2]
+    LRs$p[LRs$names == i] <- lrtest(model, tmp.mod)$Pr[2]
+  }
+  # calculate significance stars
+  stars <- c(0.001, 0.01, 0.05, 0.1)
+  names(stars) <- c("***", "**", "*", ".")
+  for (i in 1:length(stars)) {
+    LRs$stars[which(LRs$p <= stars[i] & is.na(LRs$stars))] <- names(stars)[i]
+  }
+  return(LRs)
+}
+
+# lr.calc(mod.sar.opf)
+# ms.lr
+# tmp <- data.frame(names = model.evs(mod.sar.opf), group = c("Temp", "Strat", "Chla", "Sal", "Sal", "Ocean", "Dissol", "Temp", "Temp", "Temp"))
+# lr.calc(mod.sar.opf, tmp)
+# ms.lr.group
