18e4ee6c0e024a5f8b893fc0f5187399549a517b
Isabel Fenton <isabf@nhm.ac.uk>
2014-07-25 15:35:48 +0100

    Created a palette of red-white-blue

--- palettes.R
+++ palettes.R
@@ -14,32 +14,30 @@
 
 
 ## basic colors for colorblind ---------------------------------------------
-coloring <- c("black","red","blue")
+coloring <- c("black", "red", "blue")
 
 ## percent.col; fill a certain percentage with a given colour -------------
 ## create a function that fills a certain percentage of levels with a given color. 
 ## created for Andy to colour gyres
-percent.col <- function(n.levels, main.col = "white", back.col = "grey", cutoff = 0.3){
+percent.col <- function(n.levels, main.col = "white", back.col = "grey", cutoff = 0.3) {
   cut <- round(n.levels * cutoff, 0)
   c(rep(main.col, cut), rep(back.col, n.levels - cut - 1))
 }
 
 ## heat saturation colour --------------------------------------------------
-heat.sat<-function (n, h = 1/6, s = s, v = 1, alpha = 1) {
+heat.sat <- function (n, h = 1/6, s = s, v = 1, alpha = 1) {
   if ((n <- as.integer(n[1L])) > 0) {
     n
-    if (n > 0) hsv(h, s = seq.int(from = 1/(2 * n), to = 1 - 1/(2 * n), length.out = n),
-                   v, alpha)
+    if (n > 0) hsv(h, s = seq.int(from = 1 / (2 * n), to = 1 - 1 / (2 * n), length.out = n), v, alpha)
   }
   else character()
 }
 
 ## heat v colour -----------------------------------------------------------
-heat.v<-function (n, h = 1/6, s = 1, v = v, alpha = 1) {
+heat.v <- function (n, h = 1/6, s = 1, v = v, alpha = 1) {
   if ((n <- as.integer(n[1L])) > 0) {
     n
-    if (n > 0) hsv(h, s, v = seq.int(from = 1- 1/(2 * n), to = 1/(2 * n), length.out = n)
-                   , alpha)
+    if (n > 0) hsv(h, s, v = seq.int(from = 1 - 1 / (2 * n), to = 1 / (2 * n), length.out = n), alpha)
   }
   else character()
 }
@@ -47,37 +45,61 @@ heat.v<-function (n, h = 1/6, s = 1, v = v, alpha = 1) {
 ## log heat colour ---------------------------------------------------------
 log.heat<- function (n, alpha = 1) {
   if (n < 2) {
-    n <- as.integer (n * 1000 +0.5)
+    n <- as.integer (n * 1000 + 0.5)
   }
   if ((n <- as.integer(n[1L])) > 0) {
     
-    j <- n%/%4
+    j <- n %/% 4
     i <- n - j
-    q<-round(log(seq(10,1,length.out=i),10)/6,3)
-    c(heat.sat(j, alpha = alpha),hsv(q,1,v = 1, alpha = alpha) )
+    q <- round(log(seq(10, 1, length.out = i), 10) / 6, 3)
+    c(heat.sat(j, alpha = alpha), hsv(q, 1, v = 1, alpha = alpha))
   }
   else character()
 }
 
 ## reverse log heat colour -------------------------------------------------
-rev.log.heat<- function (n, alpha = 1) {
+rev.log.heat <- function (n, alpha = 1) {
   if (n < 2) {
-    n <- as.integer (n * 1000 +0.5)
+    n <- as.integer (n * 1000 + 0.5)
   }
   if ((n <- as.integer(n[1L])) > 0) {
-    q<-round((1-log(seq(1,10,length.out=n),10))/6,4)
-    hsv(q,1,v = 1, alpha = alpha)
+    q <- round((1 - log(seq(1, 10, length.out = n), 10)) / 6, 4)
+    hsv(q, 1, v = 1, alpha = alpha)
   }
   else character()
 }
 
 ## water colours -----------------------------------------------------------
-water.colors<-function(n, alpha = 1) {
+water.colors <- function(n, alpha = 1) {
   if ((n <- as.integer(n[1L])) > 0) {
-    j <- n%/%4
-    i <- n - j*2
-    q <- round(seq(0.5,0.7,length.out = i),4)
-    c(heat.sat(j, h = 0.5, alpha = alpha),hsv(q,1, v = 1, alpha = alpha),heat.v(j, h = 0.7) )
+    j <- n %/% 4
+    i <- n - j * 2
+    q <- round(seq(0.5, 0.7, length.out = i), 4)
+    c(heat.sat(j, h = 0.5, alpha = alpha), hsv(q, 1, v = 1, alpha = alpha), heat.v(j, h = 0.7))
   }
   else character()
 }
+
+
+# rwb ---------------------------------------------------------------------
+rwb <- function (n, alpha = 1) {
+  if ((n <- as.integer(n[1L])) > 0) {
+    # if an even number
+    len <- n %/% 2
+    if (n %% 2 == 0) {
+      # reverse sequence from blue to white
+      col <- hsv(2/3, s = rev(seq.int(from = 1/(2 * len), to = 1 - 1/(2 * len), length.out = len)), 1, alpha)
+      # sequence from white to red
+      col <- c(col, hsv(1, s = seq.int(from = 1/(2 * len), to = 1 - 1/(2 * len), length.out = len), 1, alpha))
+    } else {
+      # reverse sequence from blue to white
+      col <- hsv(2/3, s = rev(seq.int(from = 1/(2 * len), to = 1 - 1/(2 * len), length.out = len)), 1, alpha)
+      # white in the middle
+      col <- c(col, hsv(0, 0, 1))
+      # sequence from white to red
+      col <- c(col, hsv(1, s = seq.int(from = 1/(2 * len), to = 1 - 1/(2 * len), length.out = len), 1, alpha))
+    } 
+  }
+  else character()
+}
+
