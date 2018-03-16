3757c705a771061ca92eae2639e912dfd00a75d7
Isabel Fenton <isabel.fenton@cantab.net>
2018-03-16 17:32:08 +0100

    Adding the ability for the compare function to resolve microperforates

--- compare.R
+++ compare.R
@@ -5,20 +5,22 @@
 ## input - species names (can be binomial or just species name)
 ## output - corrected version of that name if unknown
 
-compare<-function (forams.sample) {
+compare <- function (forams.sample, micro = FALSE) {
   # load lookup tables
-  forams.tracey<-read.csv("C:/Users/isabf/Dropbox/Documents/PhD/Project/Foraminifera/Data/traceystax.csv")
-  aze.species<-read.csv("C:/Users/isabf/Dropbox/Documents/PhD/Project/Foraminifera/Data/Traceysspecies.csv")
-  forams.lookup<-read.csv("C:/Users/isabf/Dropbox/Documents/PhD/Project/Foraminifera/Data/foramslookup.csv")
-  lookup.species<-read.csv("C:/Users/isabf/Dropbox/Documents/PhD/Project/Foraminifera/Data/lookupwodup.csv")
+  forams.tracey <- read.csv("C:/Users/isabf/Dropbox/Documents/PhD/Project/Foraminifera/Data/traceystax.csv")
+  aze.species <- read.csv("C:/Users/isabf/Dropbox/Documents/PhD/Project/Foraminifera/Data/Traceysspecies.csv")
+  forams.lookup <- read.csv("C:/Users/isabf/Dropbox/Documents/PhD/Project/Foraminifera/Data/foramslookup.csv")
+  lookup.species <- read.csv("C:/Users/isabf/Dropbox/Documents/PhD/Project/Foraminifera/Data/lookupwodup.csv")
+  forams.lookup.micro <- read.csv("C:/Users/isabf/Dropbox/Documents/PhD/Project/Foraminifera/Data/foramslookup_micro.csv")
+  lookup.species.micro <- read.csv("C:/Users/isabf/Dropbox/Documents/PhD/Project/Foraminifera/Data/lookupwodup_micro.csv")
   
   # create the matching function
-  unitestfun<-function (sample,forams,lookup) {
-    if (is.na(match(sample,forams$Species)) == FALSE) {
-      return(as.vector(forams$Full[match(sample,forams$Species)]))
+  unitestfun <- function (sample, forams, lookup) {
+    if (is.na(match(sample, forams$Species)) == FALSE) {
+      return(as.vector(forams$Full[match(sample,  forams$Species)]))
     } else {
-      if (is.na(match(sample,lookup$Neptune.names)) == FALSE) {
-        return(as.vector(lookup$Tracey.names[match(sample,lookup$Neptune.names)]))
+      if (is.na(match(sample, lookup$Neptune.names)) == FALSE) {
+        return(as.vector(lookup$Tracey.names[match(sample, lookup$Neptune.names)]))
       } else { 
         return("unknown")
       }
@@ -26,15 +28,28 @@ compare<-function (forams.sample) {
   }
   
   # run that function
-  forams.sample <- as.character(forams.sample) ## added this line
-  split<- strsplit (forams.sample," ") [[1]] [2]
-  spec<- strsplit (forams.sample," ") [[1]] [1]
+  forams.sample <- as.character(forams.sample) ## make sure it is not a factor
+  split <- strsplit(forams.sample, " ") [[1]] [2] # take the second name
+  spec <- strsplit(forams.sample, " ") [[1]] [1] # and the first name
   if (is.na(split) == FALSE) {
-    unitestfun(forams.sample,forams.tracey,forams.lookup)
+    # if there is only a species name
+    species <- unitestfun(forams.sample, forams.tracey, forams.lookup)
   } else {
-    unitestfun(spec,aze.species,lookup.species)    
+    # if the name is genus and species
+    species <- unitestfun(spec, aze.species, lookup.species)    
   }
+  # if we should resolve microperforate names
+  if (micro & species == "Micro") {
+    if (is.na(split) == FALSE) {
+      # if there is only a species name
+      species <- unitestfun(forams.sample, forams.tracey, forams.lookup.micro)
+    } else {
+      # if the name is genus and species
+      species <- unitestfun(spec, aze.species, lookup.species.micro)    
+    }
+  } 
+  return(species)
 }
 
 ## example
-# sample.names<-sapply(as.character(forams.bfd[,1]),compare,USE.NAMES=FALSE)
+# sample.names <- sapply(as.character(forams.bfd[, 1]), compare, USE.NAMES = FALSE)
